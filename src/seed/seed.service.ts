710ac6f68037542d05ee0f68d3660b49ffbd9195
Alberto Merino Guerra <albmguer@gmail.com>
2024-07-29 16:17:20 +0100

    insert multiples resgistros simultaneamente

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -20,16 +20,32 @@ export class SeedService {
   ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    await this.pokemonModel.deleteMany({}); // delete * from pokemons;
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    // const insertPromisesArray = [];
+    // data.results.forEach(async({ name, url }) => {      
+    //   const segments = url.split('/');
+    //   const no = +segments[ segments.length - 2]
+    //   // const pokemon = await this.pokemonModel.create({ name, no });
+    //   insertPromisesArray.push(
+    //     this.pokemonModel.create({ name, no })        
+    //   );
+    //   await Promise.all( insertPromisesArray );      
+    // })
+
+    const pokemonToInsert: { name: string, no: number }[] = [];
 
     data.results.forEach(async({ name, url }) => {      
+        const segments = url.split('/');
+        const no = +segments[ segments.length - 2]
+        pokemonToInsert.push({ name, no });
+    });
 
-      const segments = url.split('/');
-      const no = +segments[ segments.length - 2]
-
-      const pokemon = await this.pokemonModel.create({ name, no });
-      
-    })
+    
+    await this.pokemonModel.insertMany(pokemonToInsert)    
 
     return 'Seed Executed';
   }
