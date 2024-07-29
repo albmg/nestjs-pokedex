3cf96295d74633850d3d5ec61e5d50b36e872a52
Alberto Merino Guerra <albmguer@gmail.com>
2024-07-29 14:58:52 +0100

    Realizar peticion http desde nest & Insertar Pokemons por lote

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -6,7 +6,7 @@ export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
-  executeSeed() {
+  executeSeed() {    
     return this.seedService.executeSeed();
   }  
 }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSeedDto } from './dto/create-seed.dto';
-import { UpdateSeedDto } from './dto/update-seed.dto';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+
 
 @Injectable()
 export class SeedService {
 
-  executeSeed() {
-    return `Seed executed`
-  }
+  private readonly axios: AxiosInstance = axios;
 
+  
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    data.results.forEach(async({ name, url }) => {      
+
+      const segments = url.split('/');
+      const no = +segments[ segments.length - 2]
+
+      const pokemon = await this.pokemonModel.create({ name, no });
+      
+    })
+
+    return 'Seed Executed';
+  }
 }
