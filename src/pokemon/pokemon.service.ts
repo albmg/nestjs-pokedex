d8129913f1291fc425ddea6e0bbc7f88f84033d9
Alberto Merino Guerra <albmguer@gmail.com>
2024-07-30 14:34:35 +0100

    transform DTOs

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,6 +5,7 @@ import { Pokemon } from './entities/pokemon.entity';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -27,10 +28,17 @@ export class PokemonService {
   }
 
 
-  findAll() {
+  findAll( paginationDto: PaginationDto ) {
+
+    const { limit = 10, offset = 0 } = paginationDto;
+
     return this.pokemonModel.find()
-    .limit(5)
-    .skip(5)    
+    .limit( limit )
+    .skip( offset )    
+    .sort ({
+      no: 1
+    })
+    .select('-__v')
   }
 
   async findOne(term: string) {
